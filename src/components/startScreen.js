d83e3ccca69af7142a93c43bd0aa3957a6569755
Nihar Raote <nihar.raote@gmail.com>
2020-04-23 20:08:44 +0530

    Initialize project with a basic home screen

---
+++ src/components/startScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StartScreen = () => {
+    return (
+        <div>
+            <p>Create a group</p>
+            <p>Join an existing group</p>
+        </div>
+    )
+}
+
+export default StartScreen
